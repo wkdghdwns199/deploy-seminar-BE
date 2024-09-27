99f6ea0e7721912b800a5b80c3e86a1c55d0eacb
JangHongJoon <wkdghdwns19969@gmail.com>
2024-09-28 07:04:25 +0900

    fix : Front-End 배포 사이트 허용 추가

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000, https://cool-valkyrie-8699c4.netlify.app")
                 .allowedMethods("OPTIONS", "GET", "POST","PUT","DELETE");
     }
 }
