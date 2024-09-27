aa186be558c76f638d3d3091abdab2eb24b58ad0
JangHongJoon <wkdghdwns19969@gmail.com>
2024-09-28 08:03:35 +0900

    fix : CORS 해결을 위해 Front-End 배포 링크 허용

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000, https://deploy-seminar.netlify.app")
                 .allowedMethods("OPTIONS", "GET", "POST","PUT","DELETE");
     }
 }
